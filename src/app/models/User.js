1654c54ed4407da6165d54c3519ec0a74242d19d
Rodffer <r.rodffer@gmail.com>
2020-02-19 10:44:08 -0300

    Model de usuario

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,1 +1,19 @@
-import { Model } from 'sequelize';
+import Sequelize, { Model } from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+export default User;
