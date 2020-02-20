3601de8be7e6c3190a1613920050230caacbee5d
Rodffer <r.rodffer@gmail.com>
2020-02-19 22:00:38 -0300

    Cadastro de Usuarios

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Teste',
-    email: 'teste@teste.com',
-    password_hash: '12345teste',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
