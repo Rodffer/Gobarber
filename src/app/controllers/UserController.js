ca9cb12b3ef5a57c691b0451115116b8d7f46c14
Rodffer <r.rodffer@gmail.com>
2020-02-20 12:35:59 -0300

    Gerando hash da senha

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,7 +3,6 @@ import User from '../models/User';
 class UserController {
   async store(req, res) {
     const userExists = await User.findOne({ where: { email: req.body.email } });
-
     if (userExists) {
       return res.status(400).json({ error: 'User already exists.' });
     }
@@ -17,4 +16,5 @@ class UserController {
     });
   }
 }
+
 export default new UserController();
