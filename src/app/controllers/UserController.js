15ff6a764fced9d491364c50e9317d5bc4a7fd1d
Rodffer <r.rodffer@gmail.com>
2020-02-23 15:04:00 -0300

    Middleware de autenticação

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -15,6 +15,12 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    console.log(req.userId);
+
+    return res.json({ ok: true });
+  }
 }
 
 export default new UserController();
