9b7ea2edd8e41b5fac000a230ff544c4f2f9fe6e
Rodffer <r.rodffer@gmail.com>
2020-03-24 12:52:04 -0300

    Monitorando_falhas_na_fila/Listando_horarios_disp

---
+++ backend/src/app/controllers/AvailableController.js
@@ -0,0 +1,55 @@
+import {
+  startOfDay,
+  endOfDay,
+  setHours,
+  setMinutes,
+  setSeconds,
+  format,
+  isAfter,
+} from 'date-fns';
+import { Op } from 'sequelize';
+import Appointment from '../models/Appointment';
+import scheduleConfig from '../../config/schedule';
+
+class AvailableController {
+  async index(req, res) {
+    const { date } = req.query;
+
+    if (!date) {
+      return res.status(400).json({ error: 'Invalid date' });
+    }
+
+    const searchDate = Number(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.params.providerId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)],
+        },
+      },
+    });
+    const { schedule } = scheduleConfig;
+
+    const avaiable = schedule.map((time) => {
+      const [hour, minute] = time.split(':');
+      const value = setSeconds(
+        setMinutes(setHours(searchDate, hour), minute),
+        0
+      );
+
+      return {
+        time,
+        value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
+        avaiable:
+          isAfter(value, new Date()) &&
+          !appointments.find((a) => format(a.date, 'HH:mm') === time),
+      };
+    });
+
+    return res.json(avaiable);
+  }
+}
+
+export default new AvailableController();
